cca489b6de02d57a251f740da50d0acd09d4e264
Gy-coder <406631892@qq.com>
2023-01-27 23:29:20 +0800

    add svg loader

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { routes } from './config/router/routes'
 import { history } from './config/router/history'
+import "@svgstore";
 
 
 
